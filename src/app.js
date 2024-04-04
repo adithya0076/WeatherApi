2f8f3c935dbe40ca157df82a8a33edea1b6b8f44
adithya0076 <adithya0076@gmail.com>
2024-04-04 22:41:23 +0530

    added changes

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const cors = require("cors");
 const app = express();
 const port = 3000;
 
 const weatherroutes = require('../routes/weatherRoutes');
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use('/weather', weatherroutes);
 
